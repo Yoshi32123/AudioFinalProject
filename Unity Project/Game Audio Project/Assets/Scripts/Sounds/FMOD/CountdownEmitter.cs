c7264f9d7a67511fcf58df5dce8440db6b811cb9
Kyler <kjm7088@g.rit.edu>
2020-04-06 06:07:27 -0400

    Tweaks trying to get FMOD integration to play sounds

---
+++ Unity Project/Game Audio Project/Assets/Scripts/Sounds/FMOD/CountdownEmitter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class CountdownEmitter : MonoBehaviour
+{
+    private StudioEventEmitter emitter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
